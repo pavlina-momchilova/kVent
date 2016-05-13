4a241fd4ba8646996166b52392a47d3fc9c4565d
Rostislav <Rostislav@DESKTOP-EI4TEBB>
2016-05-13 21:44:24 +0300

    Configuring angular. CDN links are added.

---
+++ Source/kVent/Client/kVent.Client.App/app/app.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    function config($routeProvider) {
+
+        $routeProvider
+            .when('/', {
+
+            })
+            .otherwise({ redirectTo: '/' });
+    };
+
+    angular.module('kVent', [])
+        .config(['$routeProvider', config]);
+}());
---
+++ Source/kVent/Client/kVent.Client.App/scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../app/app.js" />
+/// <reference path="angular.js" />
+/// <reference path="angular-mocks.js" />
+/// <reference path="bootstrap.js" />
