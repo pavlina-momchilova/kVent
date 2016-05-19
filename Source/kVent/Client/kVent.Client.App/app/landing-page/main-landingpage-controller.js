2d737e3539c96af637bd24e9f37157cfad37183b
Rostislav <Rostislav@DESKTOP-EI4TEBB>
2016-05-19 20:13:30 +0300

    Hello World!

--- Source/kVent/Client/kVent.Client.App/app/landing-page/main-landingpage-controller.js
+++ Source/kVent/Client/kVent.Client.App/app/landing-page/main-landingpage-controller.js
@@ -1,1 +1,13 @@
-﻿
+﻿(function () {
+
+    'use strict';
+
+    function MainLandingPageController() {
+        var vm = this;
+
+        vm.helloWorld = "Hello World!";
+    }
+
+    angular.module('kVent.controllers')
+        .controller('MainLandingPageController', [MainLandingPageController]);
+}());
